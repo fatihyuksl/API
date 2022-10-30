7bd5628272db126119f1c7f5b3b613936467591b
fatihhhh <fatihisiteknik42@gmail.com>
2022-10-30 22:44:23 +0300

    v06

--- src/test/java/Get02b.java
+++ src/test/java/Get02b.java
@@ -17,7 +17,7 @@ public class Get02b extends ReqresBaseUrl {
      And
          Server is "cloudflare"
      And
-         Response body should be empty12345
+         Response body should be empty123456
   */
     @Test
     public void get02(){
