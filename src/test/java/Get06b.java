08fdcbefa7613f29112c0994051513363c907d3f
fatihhhh <fatihisiteknik42@gmail.com>
2022-10-31 22:26:32 +0300

    v06

--- src/test/java/Get06b.java
+++ src/test/java/Get06b.java
@@ -3,7 +3,10 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Test;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertEquals;
 
 public class Get06b  extends ReqresBaseUrl {
@@ -37,16 +40,20 @@ public class Get06b  extends ReqresBaseUrl {
         //        2)Print all pantone_values
         System.out.println(jsonPath.getList("data.pantone_value"));
         System.out.println(jsonPath.getList("data.pantone_value").get(0));
+        response.then().body("data",hasSize(6));
         //        3)Print all ids greater than 3 on the console
-        jsonPath.getList("data.findAll {it.id>3}.name");
+        List<Integer> ids = jsonPath.getList("data.findAll{it.id>3}.id");
+        System.out.println("ids = " + ids);
 
+//        Assert that there are 3 ids greater than 3
+        assertEquals(3,ids.size());
 
-        //        Assert that there are 3 ids greater than 3
+//        4)Print all names whose ids are less than 3 on the console
+        List<String> names = jsonPath.getList("data.findAll{it.id<3}.name");
+        System.out.println("names = " + names);
 
-        //        4)Print all names whose ids are less than 3 on the console
-
-        //        Assert that the number of names whose ids are less than 3 is 2
-
+//        Assert that the number of names whose ids are less than 3 is 2
+        assertEquals(2,names.size());
 
     }
 
