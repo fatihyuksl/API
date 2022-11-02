bc8219a2e507867fc9cef6000d7e9e648fc9a3ff
fatihhhh <fatihisiteknik42@gmail.com>
2022-11-02 22:22:51 +0300

    v06

--- src/test/java/get_requests/Get09.java
+++ src/test/java/get_requests/Get09.java
@@ -48,7 +48,7 @@ public class Get09 extends RestfulBaseUrl {
                 "bookingdates.checkout",equalTo("2014-10-23"),
                 "additionalneeds",equalTo("Breakfast"));
 
-        //2.yol jsonPath ile234
+        //2.yol jsonPath ile2345
         JsonPath json=response.jsonPath();
         SoftAssert softAssert= new SoftAssert();
         softAssert.assertEquals(json.getString("firstname"),"Sally");
